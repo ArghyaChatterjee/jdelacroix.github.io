dcd78e904932459d252bc3640282420066039fbb
Jean-Pierre de la Croix <ajpl.delacroix@gmail.com>
2014-08-31 15:06:48 -0400

    Fixed contact form validation.

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-    $('#contactForm').bootstrapValidator();
+    $('#contactForm').bootstrapValidator({
+      onSuccess: function(event) {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+          $('#success > .alert-success')
+              .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+              .append('</div>');
+
+          //clear all fields
+          $('#contactForm').trigger("reset");
+      }
+    });
 });
 
 /*$(function() {
@@ -44,6 +58,15 @@ $(document).ready(function() {
 });
 
 */
+
+/* Cancel contact form entry */
+function cancel_form() {
+  $('#contactForm').data('bootstrapValidator').resetForm();
+  $('#contactForm').trigger("reset");
+  $('div.controls').toggleClass('floating-label-form-group-with-value', false);
+  $('div.controls').toggleClass('floating-label-form-group', true);
+  toggle_visibility("contact", "off");
+}
 
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
